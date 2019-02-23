e3bca3a27e4efa0f1cba321700e80fa303f076a9
blubrandi <blubrandi0@gmail.com>
2019-02-23 12:24:30 -0700

    Services JS tab component functionality working.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // JS goes here
+
+class TabLink {
+    constructor(element) {
+        this.element = element
+        this.data = this.element.dataset.tab 
+        this.item = document.querySelector(`.tabs-item[data-tab='${this.data}']`)
+        this.tabItem = new TabItem(this.item)
+        this.element.addEventListener('click', () => {
+            this.select()
+        })
+}
+
+    select() {
+        const links = document.querySelectorAll('.tabs-link')
+        links.forEach(link => {
+            link.classList.remove('selected')})
+        this.element.classList.add('selected')
+        this.tabItem.select()
+    }
+}
+
+class TabItem {
+    constructor(element) {
+        this.element = element
+    }
+
+    select() {
+        const items = document.querySelectorAll('.tabs-item')
+        items.forEach(item => {
+            item.classList.remove('selected')
+        })
+        this.element.classList.add('selected')
+    }
+}
+let links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link))
